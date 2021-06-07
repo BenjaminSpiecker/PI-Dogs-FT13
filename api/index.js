0cd00ddf71ef9513e11f84896863894e1d35ed5f
benjaminspiecker <benjaminspiecker@gmail.com>
2021-06-07 19:00:27 -0300

    segundo commit

--- api/index.js
+++ api/index.js
@@ -18,11 +18,51 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn, Breed, Temperament } = require('./src/db.js');
+const axios = require('axios');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  let breeds, searchId;
+
+  breeds = await axios.get('https://api.thedogapi.com/v1/breeds');
+  console.log(breeds.data[0]);
+
+  searchId = await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${breeds.data[0].name}`);
+  console.log(searchId.data[0]);
+
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
+
+/* 
+const t1 = await Temperament.create({ name: "Stubborn" });
+  const t2 = await Temperament.create({ name: "Curious" });
+  const t3 = await Temperament.create({ name: "Playful" });
+  const t4 = await Temperament.create({ name: "Adventurous" });
+  const t5 = await Temperament.create({ name: "Active" });
+
+  const breed1 = await Breed.create({
+    name: "Affenpinscher",
+    height: "23 - 29",
+    weight:"3 - 6",
+    life_span: "10 - 12 years",
+  });
+
+  const breed2 = await Breed.create({
+    name: "Afghan Hound",
+    height: "23 - 29",
+    weight: "3 - 6",
+    life_span: "10 - 12 years",
+  });
+
+  await breed1.addTemperament([t1,t2,t3]);
+  await breed2.addTemperament([t4,t5]);
+
+  let busquedaAll = await Breed.findAll({
+    include: {model: Temperament}
+  });
+
+  // await console.log(JSON.stringify(busquedaAll, null, 2));
+*/
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -9,4 +9,28 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
   });
+
+  sequelize.define('breed', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.STRING
+    }
+  }, {tableName: 'breeds'});
+
+  sequelize.define('temperament', {
+    name: {
+      type: DataTypes.STRING
+    }
+  }, {tableName: 'temperaments'});
 };
